6d64deaf674e33a7cdc8b40e5d5a4bf1aad363bd
mariowettig <m.wettig@student.maastrichtuniversity.nl>
2024-05-17 18:03:05 +0200

    documentation of files

---
+++ src/test/java/data_management/PateintRecordTest.java
@@ -0,0 +1,14 @@
+package data_management;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.data_management.DataStorage;
+import com.data_management.PatientRecord;
+
+import java.util.List;
+
+public class PateintRecordTest {
+}
