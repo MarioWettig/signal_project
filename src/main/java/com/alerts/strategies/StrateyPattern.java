3c747af8bfdcbc8dc89a2e9199e5afa06d0ded42
lucabighignoli <l.bighignoli@student.maastrichtuniversity.nl>
2024-06-09 10:08:32 +0200

    OxygenSaturationStrategy

--- src/main/java/com/alerts/strategies/StrateyPattern.java
+++ src/main/java/com/alerts/strategies/StrateyPattern.java
@@ -1,7 +1,8 @@
 package com.alerts.strategies;
 
 import com.alerts.alerts.Alert;
+import com.data_management.Patient;
 
 public interface StrateyPattern {
-    public Alert checkAlert();
+    public Alert checkAlert(Patient patient);
 }
